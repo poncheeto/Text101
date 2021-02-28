1dbfabed04c2c1b8a97f675af3dfc143cc38b2e7
Poncho <jplyles1212@gmail.com>
2021-02-27 22:23:34 -0800

    Add text script

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Long, long ago...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
